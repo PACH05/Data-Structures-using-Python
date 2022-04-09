59dcb870738a787f126f6f4eba2824fa9b287081
Prasanta Acharyya <acharyya.prasanta05@gmail.com>
2022-04-09 22:59:01 +0530

    code added

--- Array(List).py
+++ Array(List).py
@@ -1,9 +1,12 @@
 # Array(list) operations
 
 li =[1,2,8,9,7,50,40,3]
+#Adds the element at the end
 li.append(4)
 print(li)
+#Removes the given element
 li.remove(8)
 print(li)
+#Sorts the list in non-decreasing order
 li.sort()
 print(li)
